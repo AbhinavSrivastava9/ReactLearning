cb2998cd2b1ce24694b2e18647b3a5e08cacb018
AbhinavSrivastava9 <abhinav.srivastava.bhs@gmail.com>
2022-01-23 21:03:07 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>lets get started and work hard on it..</h2>
+      <p>This is also visible</p>
+    </div>
+  );
+}
+
+export default App;
